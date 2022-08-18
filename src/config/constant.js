3a906d8f867e6fc9ea616be3c26cf067a6828f02
DeepakNagarkoti <deepak.nagarkoti95@gmail.com>
2022-08-18 22:08:57 +0530

    feat: added new constant

--- src/config/constant.js
+++ src/config/constant.js
@@ -1,2 +1,2 @@
-//module.exports.URL_BASENAME = 'http://odinvr-backend.herokuapp.com/'
-module.exports.URL_BASENAME = 'http://192.168.1.14:3000/'
+module.exports.URL_BASENAME = 'http://13.41.227.34:3000/'
+// module.exports.URL_BASENAME = 'http://192.168.1.14:3000/'
